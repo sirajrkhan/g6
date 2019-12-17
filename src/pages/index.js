dcac356e1c1cf88e3038ab86378e502cf3664a05
Siraj Khan <sirajrkhan@live.in>
2019-12-17 11:51:50 +0530

    added reactstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'reactstrap/dist/reactstrap.min'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Navi from "../components/navcomp"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Navi />
     <h1>Hello!!</h1>
     <p>Let's build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
